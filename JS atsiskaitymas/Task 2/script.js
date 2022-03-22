c38b013ebcf6a788b51e5d8f6785378d6b5d3a10
BenKer <benediktas.kertenis@codeacademylt.onmicrosoft.com>
2022-03-22 09:00:50 +0200

    Task 2

--- JS atsiskaitymas/Task 2/script.js
+++ JS atsiskaitymas/Task 2/script.js
@@ -3,3 +3,10 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+let clickedTimes = 0;
+
+document.querySelector("#btn__element").addEventListener("click", function () {
+  clickedTimes++;
+  document.querySelector("#btn__state").innerHTML = clickedTimes;
+});
